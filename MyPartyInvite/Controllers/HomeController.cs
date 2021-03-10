41f52e6fe5de67527d6ad197ee252d90ae091312
Paulo Barbosa <pflsbarbosa@gmail.com>
2021-03-10 12:23:49 +0000

    Your First MVC App from Pro ASP.NET MVC5

---
+++ MyPartyInvite/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿
+using PartyInvite.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PartyInvite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greetings = hour < 12 ? "Good Morning" : "Good  Afternoon";
+            return View();
+        }
+        [HttpGet]
+         public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: Email response to the party organizer
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                // there is a validation error
+                return View();
+            }
+        }       
+    }
+}
